092a7d0c17e2bc9fa12e26e573dcdf773d9f3d57
Sanghyuk <saanghyuk@gmail.com>
2018-03-09 14:32:59 +0900

    3/9

---
+++ School/3:9.R
@@ -0,0 +1,50 @@
+mode(3)
+mode("3")
+mode(T)
+mode("T")
+
+
+mid <- matrix(c(1,2,3,4,5,6), ncol=2, dimnames=list(c("kim", "kang", "shin"), c("math", "english")))
+
+lst <- list(name="KIM GILDONG", dept="Sales", salary=4000000, union=T)
+lst
+
+lst$name
+lst['dept']
+
+lst[['salary']]+3
+
+
+
+mid
+
+mode(array1)
+ 
+z <- sqrt(9)
+z
+
+
+x <- rnorm(100, mean=0, sd=1)
+
+install.packages('ggplot2')
+
+hist(x)
+
+
+#환경창 -> 변수-값이 계속 축적. 
+
+
+rm(list=ls()) #--> 환경창 현재 할당된 변수 전체 삭제. 
+a <- rnorm(1000)
+head(a)
+
+x <-as.complex(2)
+x     
+
+integer(3)
+double(3)
+mode(3)  
+
+
+xy <- c(1,2,3)
+yz <- c(4,5,6)_
